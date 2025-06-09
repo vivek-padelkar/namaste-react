f7d64e440e89a091e94dc0da6fca27589cd1a68c
vivekpadelkar <vivek.padelkar@gmail.com>
2025-06-09 19:11:03 +0530

    installed parcel and made prod build

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const parent = React.createElement('div', { id: 'parent' },
-    React.createElement('div', { id: 'child' },
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const parent = React.createElement('div', { id: 'parent', key: 'parent' },
+    React.createElement('div', { id: 'child', key: 'child' },
         [
-            React.createElement('h1', { id: 'h1-element' }, 'Hello, world! using H1'),
-            React.createElement('h2', { id: 'h2-element' }, 'Hello, world! using H2'),
-            React.createElement('p', { id: 'p-element' }, 'This is a paragraph element.')
+            React.createElement('h1', { id: 'h1-element', key: 'h1ele' }, 'Hello, world! using H1'),
+            React.createElement('h2', { id: 'h2-element', key: 'h2ele' }, 'Hello, world! using H2'),
+            React.createElement('p', { id: 'p-element', key: 'pele' }, 'This is a paragraph element.')
         ]
     )
 );
